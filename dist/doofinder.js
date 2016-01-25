e1ac1d4f901edd9bfdc9237b0f959b813e7839ab
Kike Coslado <kike@doofinder.com>
2016-01-25 17:41:23 +0100

    url-params helper with query

--- dist/doofinder.js
+++ dist/doofinder.js
@@ -899,7 +899,7 @@ author: @ecoslado
 },{"./util/jquery":6,"qs":87}],3:[function(_dereq_,module,exports){
 (function() {
   module.exports = {
-    version: "1.0.1",
+    version: "1.0.2",
     Client: _dereq_("./client"),
     Handlebars: _dereq_("handlebars"),
     Widget: _dereq_("./widget"),
--- src/doofinder.coffee
+++ src/doofinder.coffee
@@ -1,5 +1,5 @@
 module.exports = 
-  version: "1.0.1"  
+  version: "1.0.2"  
   Client: require "./client"
   Handlebars: require "handlebars"
   Widget: require "./widget"
