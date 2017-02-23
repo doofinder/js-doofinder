7db214c3ca1e9beeb8153a3298d6a332c38d866c
Carlos Escribano <carlos@doofinder.com>
2017-02-23 09:03:18 +0100

    Smother scroll?
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- dist/doofinder.js
+++ dist/doofinder.js
@@ -1590,7 +1590,7 @@ author: @ecoslado
       callback: function() {},
       scrollOffset: 200,
       content: container.children().first(),
-      throttle: 150
+      throttle: 100
     };
     options = extend(true, defaults, options || {});
     content = $(options.content);
