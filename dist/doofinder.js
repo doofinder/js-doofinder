4b6b88db58fb622fba6533af136e122094ff70ba
Carlos Escribano <carlos@doofinder.com>
2017-12-22 12:25:32 +0100

    Small refactoring and docs for Debouncer class
    Signed-off-by: Carlos Escribano <carlos@doofinder.com>

--- dist/doofinder.js
+++ dist/doofinder.js
@@ -1278,28 +1278,44 @@
 
   requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame;
 
+
+  /**
+   * Avoids handling an event every time it fires, asking the browser to do it.
+   * Copied and modified from: github:WickyNilliams/headroom.js's Debouncer.js
+   */
+
   Debouncer = (function() {
+
+    /**
+     * Instantiates a new Debouncer.
+     * @param  {Function} callback Event handler to be executed.
+     */
     function Debouncer(callback) {
       this.callback = callback;
       this.ticking = false;
     }
 
-    Debouncer.prototype.update = function() {
-      if (typeof this.callback === "function") {
-        this.callback();
-      }
-      return this.ticking = false;
-    };
 
-    Debouncer.prototype.tick = function() {
+    /**
+     * Event handler method, it's executed always instead of the original
+     * callback.
+     */
+
+    Debouncer.prototype.handleEvent = function() {
       if (!this.ticking) {
         requestAnimationFrame(this.boundCallback || (this.boundCallback = this.update.bind(this)));
         return this.ticking = true;
       }
     };
 
-    Debouncer.prototype.handleEvent = function() {
-      return this.tick();
+
+    /**
+     * Executes the configured callback and re-enables the callback to be queued.
+     */
+
+    Debouncer.prototype.update = function() {
+      this.callback();
+      return this.ticking = false;
     };
 
     return Debouncer;
--- src/util/debouncer.coffee
+++ src/util/debouncer.coffee
@@ -1,20 +1,31 @@
-# https://github.com/WickyNilliams/headroom.js/blob/3282c23/src/Debouncer.js
 requestAnimationFrame = window.requestAnimationFrame or window.webkitRequestAnimationFrame or window.mozRequestAnimationFrame
 
+###*
+ * Avoids handling an event every time it fires, asking the browser to do it.
+ * Copied and modified from: github:WickyNilliams/headroom.js's Debouncer.js
+###
 class Debouncer
+  ###*
+   * Instantiates a new Debouncer.
+   * @param  {Function} callback Event handler to be executed.
+  ###
   constructor: (@callback) ->
     @ticking = false
 
-  update: ->
-    @callback?()
-    @ticking = false
-
-  tick: ->
+  ###*
+   * Event handler method, it's executed always instead of the original
+   * callback.
+  ###
+  handleEvent: ->
     unless @ticking
       requestAnimationFrame @boundCallback or (@boundCallback = @update.bind @)
       @ticking = true
 
-  handleEvent: ->
-    @tick()
+  ###*
+   * Executes the configured callback and re-enables the callback to be queued.
+  ###
+  update: ->
+    @callback()
+    @ticking = false
 
 module.exports = Debouncer
