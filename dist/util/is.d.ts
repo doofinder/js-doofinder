2af36a7456aa99f73afd20879d6947adc21eeb64
Carlos Escribano <carlos@doofinder.com>
2019-10-17 12:51:22 +0200

    build: remove dist from repo

--- dist/util/is.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const isArray: (value: unknown) => boolean;
-export declare const isObject: (value: unknown) => boolean;
-export declare const isPlainObject: (value: unknown) => boolean;
-export declare const isNull: (element: unknown) => boolean;
