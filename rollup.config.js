c36f0ce407f394d46c7d312d9a93fa0ea61b66fd
Carlos Escribano <carlos@doofinder.com>
2019-10-17 13:28:38 +0200

    build: refactor bundling

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
-import builtins from 'rollup-plugin-node-builtins';
 import clear from 'rollup-plugin-clear';
-import commonjs from 'rollup-plugin-commonjs';
-import globals from 'rollup-plugin-node-globals';
 import inject from 'rollup-plugin-inject';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
+
+function defaultPlugins(file) {
+  return [
+    clear({
+      targets: [file],
+      watch: true
+    }),
+    resolve({
+      dedupe: ['qss']
+    }),
+    typescript()
+  ]
+}
 
 export default [
   {
@@ -13,20 +23,7 @@ export default [
       file: 'lib/index.mjs',
       format: 'esm'
     },
-    plugins: [
-      clear({
-        targets: ['lib/index.mjs'],
-        watch: true
-      }),
-      resolve({
-        dedupe: ['qs'],
-        preferBuiltins: true
-      }),
-      commonjs({
-        include: 'node_modules/**'
-      }),
-      typescript()
-    ]
+    plugins: defaultPlugins('lib/index.mjs')
   },
   {
     input: 'src/index.ts',
@@ -38,27 +35,13 @@ export default [
       'node-fetch',
       'qs'
     ],
-    plugins: [
-      clear({
-        targets: ['lib/index.js'],
-        watch: true
-      }),
-      resolve({
-        dedupe: ['qs'],
-        preferBuiltins: true
-      }),
-      commonjs({
-        include: 'node_modules/**'
-      }),
-      typescript(),
-      globals(),
-      builtins(),
+    plugins: defaultPlugins('lib/index.js').concat([
       inject({
         exclude: 'node_modules/**',
         modules: {
           fetch: 'node-fetch'
         }
       })
-    ]
+    ])
   }
 ]
