568e24fb7cc625a00fcffcf83e61512a3435a2d2
Carlos Escribano <carlos@doofinder.com>
2019-10-17 12:32:17 +0200

    build: refactor build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,47 +1,64 @@
-const fs = require('fs');
-
+import builtins from 'rollup-plugin-node-builtins';
 import clear from 'rollup-plugin-clear';
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import globals from 'rollup-plugin-node-globals';
-import builtins from 'rollup-plugin-node-builtins';
+import inject from 'rollup-plugin-inject';
+import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
-import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: ['src/doofinder.ts'],
-  output: [
-    {
-      file: 'dist/doofinder.min.js',
-      format: 'esm',
-      name: 'doofinder'
+export default [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/index.mjs',
+      format: 'esm'
     },
-    {
-      file: 'dist/doofinder.min.cjs.js',
-      format: 'cjs',
-      name: 'doofinder'
+    plugins: [
+      clear({
+        targets: ['lib/index.mjs'],
+        watch: true
+      }),
+      resolve({
+        dedupe: ['qs'],
+        preferBuiltins: true
+      }),
+      commonjs({
+        include: 'node_modules/**'
+      }),
+      typescript()
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/index.js',
+      format: 'cjs'
     },
-    {
-      file: 'dist/doofinder.min.system.js',
-      format: 'system',
-      name: 'doofinder'
-    }
-  ],
-  plugins: [
-    clear({
-      targets: ['dist'],
-      watch: true
-    }),
-    resolve({
-      dedupe: [ 'qs' ],
-      preferBuiltins: true
-    }),
-    commonjs({
-      include: 'node_modules/**'
-    }),
-    globals(),
-    builtins(),
-    typescript(),
-    terser()
-  ],
-}
+    external: [
+      'node-fetch',
+      'qs'
+    ],
+    plugins: [
+      clear({
+        targets: ['lib/index.js'],
+        watch: true
+      }),
+      resolve({
+        dedupe: ['qs'],
+        preferBuiltins: true
+      }),
+      commonjs({
+        include: 'node_modules/**'
+      }),
+      typescript(),
+      globals(),
+      builtins(),
+      inject({
+        exclude: 'node_modules/**',
+        modules: {
+          fetch: 'node-fetch'
+        }
+      })
+    ]
+  }
+]
