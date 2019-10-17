6d756b7190129f949fa3e3f1cbe60ad0b6cf4c29
Carlos Escribano <carlos@doofinder.com>
2019-10-17 16:50:35 +0200

    build: iife build + demo refactoring

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,19 @@ import inject from 'rollup-plugin-inject';
 import resolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 
-function defaultPlugins(file) {
+function defaultPlugins(file, options = {}) {
   return [
     clear({
       targets: [file],
       watch: true
     }),
     resolve({
-      dedupe: ['qss']
+      dedupe: ['qss'],
+      browser: options.browser || false
     }),
-    typescript()
+    typescript({
+      typescript: require('typescript')
+    })
   ]
 }
 
@@ -28,12 +31,21 @@ export default [
   {
     input: 'src/index.ts',
     output: {
+      file: 'lib/doofinder.js',
+      format: 'iife',
+      name: 'doofinder'
+    },
+    plugins: defaultPlugins('lib/doofinder.js', { browser: true })
+  },
+  {
+    input: 'src/index.ts',
+    output: {
       file: 'lib/index.js',
       format: 'cjs'
     },
     external: [
       'node-fetch',
-      'qs'
+      'qss'
     ],
     plugins: defaultPlugins('lib/index.js').concat([
       inject({
