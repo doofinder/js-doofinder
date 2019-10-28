642906da39798df0ce59a279fab7aa8cee51f874
Carlos Escribano <carlos@doofinder.com>
2019-10-28 17:58:19 +0100

    refactor: introspection utils (#182)
    - isNull does not actually add too much
    - Added missing introspection functions

--- src/util/is.ts
+++ src/util/is.ts
@@ -1,12 +1,14 @@
 import { Zone } from '../types';
 
+export const isString = (value: unknown): boolean => Object.prototype.toString.call(value) === '[object String]';
+
+export const isNumber = (value: unknown): boolean => Object.prototype.toString.call(value) === '[object Number]';
+
 export const isArray = (value: unknown): boolean => Array.isArray(value);
 
 export const isObject = (value: unknown): boolean => Object.prototype.toString.call(value) === '[object Object]';
 
 export const isPlainObject = (value: unknown): boolean =>
   isObject(value) && value.constructor === Object && !(value as Node).nodeType && !(value as Window).setInterval;
-
-export const isNull = (element: unknown): boolean => !(typeof element !== 'undefined' && element !== null);
 
 export const isValidZone = (zone: string): boolean => Object.values(Zone).includes(zone as Zone);
