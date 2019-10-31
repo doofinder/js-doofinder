4e8fc5ee8e2d8d0e644266efb6d58c2a4b44e349
José Carlos Cuevas <reset.reboot@gmail.com>
2019-10-31 11:13:37 +0100

    refactor(types): move Facet types to request where they belong

--- src/types/request.ts
+++ src/types/request.ts
@@ -5,6 +5,31 @@ export interface RangeFilter {
   to: number;
 }
 
+/**
+ * Defines the fields for a facet
+ * that is a range. Beware, we need
+ * either one of the lt or lte fields
+ * and either one of the gt or gte fields
+ */
+export interface RangeFacet {
+  lte?: number;
+  gte?: number;
+  lt?: number;
+  gt?: number;
+}
+
+/**
+ * Type definition of a single facet
+ */
+export type FacetOption = RangeFacet | string[] | number[];
+
+/**
+ * Type holding all the current facets
+ */
+export interface Facet {
+  [facetName: string]: FacetOption;
+}
+
 export type TermsFilter = string | string[];
 
 /**
