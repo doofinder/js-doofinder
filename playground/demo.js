7c65b7c5b6d010567aede20574811e48727a35ce
Carlos Escribano <carlos@doofinder.com>
2019-12-16 16:41:57 +0100

    chore: fix playground

--- playground/demo.js
+++ playground/demo.js
@@ -1,6 +1,6 @@
 function demo({ Client, Query, Zone }) {
   const client = new Client({ zone: Zone.EU });
-  const query = new Query('c0604b71c273c1fb3ef13eb2adfa4452');
+  const query = new Query({hashid: 'c0604b71c273c1fb3ef13eb2adfa4452'});
 
   query.searchText('silla');
   query.addFilter('brand', 'JANE');
