4c3732fc76bdab097ce58870246e4ff8ae6445c0
Carlos Escribano <carlos@doofinder.com>
2019-12-23 13:13:48 +0100

    New Version: v5.10.1

--- dist/doofinder.js
+++ dist/doofinder.js
@@ -926,7 +926,7 @@
 },{"./client":1,"./util/errors":7,"./util/eventEnabled":8,"./util/freezer":9,"./util/merge":12,"./util/thing":15,"./widgets/widget":23,"qs":48}],3:[function(require,module,exports){
 (function() {
   module.exports = {
-    version: "5.10.0",
+    version: "5.10.1",
     Client: require("./client"),
     Controller: require("./controller"),
     Stats: require("./stats"),
