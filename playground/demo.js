1a314ef553ecbcdc06c42e2bd0b091a579a5fcc5
Carlos Escribano <carlos@doofinder.com>
2020-01-13 14:09:47 +0100

    chore: fix demo

--- playground/demo.js
+++ playground/demo.js
@@ -1,5 +1,5 @@
 function demo({ Client, Query, Zone }) {
-  const client = new Client({ zone: Zone.EU });
+  const client = new Client({ zone: Zone.EU1 });
   const query = new Query({hashid: 'c0604b71c273c1fb3ef13eb2adfa4452'});
 
   query.searchText('silla');
