3f59333bf8348dbd6898a17ebaec920f53e2b7b8
Carlos Escribano <carlos@doofinder.com>
2020-03-04 22:56:35 +0100

    fix: demo

--- playground/demo.js
+++ playground/demo.js
@@ -2,7 +2,7 @@ function demo({ Client, Query, Zone }) {
   const client = new Client({ zone: Zone.EU1 });
   const request = new Query({hashid: 'c0604b71c273c1fb3ef13eb2adfa4452'});
 
-  request.query = 'silla';
+  request.text = 'silla';
   request.filters.add('brand', 'JANE');
   request.filters.add('brand', 'RECARO');
   request.queryCounter = 2;
