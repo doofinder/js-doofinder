20bd2cb6af2a5061a285802192622c9fb4e3491b
Carlos Escribano <carlos@doofinder.com>
2020-03-18 10:59:50 +0100

    chore: small fix

--- playground/demo.js
+++ playground/demo.js
@@ -1,5 +1,5 @@
-function demo({ Client, Query, Zone }) {
-  const client = new Client({ zone: Zone.EU1 });
+function demo({ Client, Query }) {
+  const client = new Client({ zone: 'eu1' });
   const request = new Query({hashid: 'c0604b71c273c1fb3ef13eb2adfa4452'});
 
   request.text = 'silla';
