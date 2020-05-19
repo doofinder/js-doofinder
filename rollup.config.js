d63534e7b2e34839ca6b59215ef6aa642981f804
Carlos Escribano <carlosescri@gmail.com>
2020-05-19 15:42:27 +0200

    Revert "fix: fix rollup build (#257)"
    This reverts commit fb4adf5867afc74f8f1c591a9fbdc140763de127.

--- rollup.config.js
+++ rollup.config.js
@@ -2,70 +2,58 @@ import clear from 'rollup-plugin-clear';
 import commonjs from '@rollup/plugin-commonjs';
 import inject from '@rollup/plugin-inject';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
 
 function defaultPlugins(file, options = {}) {
-  const plugins = [
+  return [
     clear({
       targets: [file],
-      watch: true,
+      watch: true
     }),
     resolve({
-      browser: options.browser || false,
+      browser: options.browser || false
     }),
-    typescript(),
-    commonjs({ extensions: ['.js', '.ts'] }), // the ".ts" extension is required
-  ];
-  if (options.terser) plugins.push(terser());
-  return plugins;
+    commonjs(),
+    typescript({
+      typescript: require('typescript')
+    })
+  ]
 }
 
 export default [
-  // ES module
   {
     input: 'src/index.ts',
     output: {
-      file: 'lib/doofinder.mjs',
-      format: 'esm',
+      file: 'lib/index.mjs',
+      format: 'esm'
     },
-    plugins: defaultPlugins('lib/doofinder.mjs'),
+    plugins: defaultPlugins('lib/index.mjs')
   },
-  // for browsers, all included
   {
     input: 'src/index.ts',
     output: {
       file: 'lib/doofinder.js',
       format: 'iife',
-      name: 'doofinder',
+      name: 'doofinder'
     },
-    plugins: defaultPlugins('lib/doofinder.js', { browser: true }),
+    plugins: defaultPlugins('lib/doofinder.js', { browser: true })
   },
-  // for browsers, all included and minified
   {
     input: 'src/index.ts',
     output: {
-      file: 'lib/doofinder.min.js',
-      format: 'iife',
-      name: 'doofinder',
+      file: 'lib/index.js',
+      format: 'cjs'
     },
-    plugins: defaultPlugins('lib/doofinder.min.js', { browser: true, terser: true }),
-  },
-  // Commonjs module for old node (not ES modules)
-  {
-    input: 'src/index.ts',
-    output: {
-      file: 'lib/doofinder.common.js',
-      format: 'cjs',
-    },
-    external: ['node-fetch'],
-    plugins: defaultPlugins('lib/doofinder.common.js').concat([
+    external: [
+      'node-fetch',
+    ],
+    plugins: defaultPlugins('lib/index.js').concat([
       inject({
         exclude: 'node_modules/**',
         modules: {
-          fetch: 'node-fetch',
-        },
-      }),
-    ]),
-  },
-];
+          fetch: 'node-fetch'
+        }
+      })
+    ])
+  }
+]
