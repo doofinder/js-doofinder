78dd250c44bf03db4b21c295535aa8be27e0e43b
Johanderson Mogollon <johander1822@gmail.com>
2020-05-19 15:42:14 +0200

    fix: fix rollup build (#257)
    Now the library works with Webpack.

--- rollup.config.js
+++ rollup.config.js
@@ -2,58 +2,70 @@ import clear from 'rollup-plugin-clear';
 import commonjs from '@rollup/plugin-commonjs';
 import inject from '@rollup/plugin-inject';
 import resolve from '@rollup/plugin-node-resolve';
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
+import { terser } from 'rollup-plugin-terser';
 
 function defaultPlugins(file, options = {}) {
-  return [
+  const plugins = [
     clear({
       targets: [file],
-      watch: true
+      watch: true,
     }),
     resolve({
-      browser: options.browser || false
+      browser: options.browser || false,
     }),
-    commonjs(),
-    typescript({
-      typescript: require('typescript')
-    })
-  ]
+    typescript(),
+    commonjs({ extensions: ['.js', '.ts'] }), // the ".ts" extension is required
+  ];
+  if (options.terser) plugins.push(terser());
+  return plugins;
 }
 
 export default [
+  // ES module
   {
     input: 'src/index.ts',
     output: {
-      file: 'lib/index.mjs',
-      format: 'esm'
+      file: 'lib/doofinder.mjs',
+      format: 'esm',
     },
-    plugins: defaultPlugins('lib/index.mjs')
+    plugins: defaultPlugins('lib/doofinder.mjs'),
   },
+  // for browsers, all included
   {
     input: 'src/index.ts',
     output: {
       file: 'lib/doofinder.js',
       format: 'iife',
-      name: 'doofinder'
+      name: 'doofinder',
     },
-    plugins: defaultPlugins('lib/doofinder.js', { browser: true })
+    plugins: defaultPlugins('lib/doofinder.js', { browser: true }),
   },
+  // for browsers, all included and minified
   {
     input: 'src/index.ts',
     output: {
-      file: 'lib/index.js',
-      format: 'cjs'
+      file: 'lib/doofinder.min.js',
+      format: 'iife',
+      name: 'doofinder',
     },
-    external: [
-      'node-fetch',
-    ],
-    plugins: defaultPlugins('lib/index.js').concat([
+    plugins: defaultPlugins('lib/doofinder.min.js', { browser: true, terser: true }),
+  },
+  // Commonjs module for old node (not ES modules)
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'lib/doofinder.common.js',
+      format: 'cjs',
+    },
+    external: ['node-fetch'],
+    plugins: defaultPlugins('lib/doofinder.common.js').concat([
       inject({
         exclude: 'node_modules/**',
         modules: {
-          fetch: 'node-fetch'
-        }
-      })
-    ])
-  }
-]
+          fetch: 'node-fetch',
+        },
+      }),
+    ]),
+  },
+];
