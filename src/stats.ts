248c5fbffa05854ef6d10bdd680051ce890beb65
Manuel Irazabal <manuelirazabal@hotmail.com>
2023-07-31 17:48:44 +0200

    feat: 1753 Search endpoints part 1

--- src/stats.ts
+++ src/stats.ts
@@ -1,7 +1,6 @@
 import { Client } from './client';
 import { validateRequired, validateDoofinderId, ValidationError } from './util/validators';
 import { clone } from './util/clone';
-import { Method } from './enum/method';
 
 /**
  * Basic parameters for stats requests.
@@ -15,38 +14,31 @@ export interface StatsParams {
 }
 
 /**
- * Basic parameters for init session requests.
- * @public
- */
-export interface InitParams extends StatsParams {
-  /** Unique ID of the user. */
-  user_id?: string;
-}
-
-/**
- * Basic parameters for checkout requests.
- * @public
- */
-export interface CheckoutParams extends StatsParams {
-  /** Unique ID of the user. */
-  user_id?: string;
-}
-
-/**
  * Parameters for click stats with dfid.
  * @public
  */
 export interface ClickStatsParamsWithDfid extends StatsParams {
-  /** Optional ID of the custom results that produced the current set of results. */
-  custom_results_id?: string | number;
   /** Unique ID of the clicked result. */
   dfid: string;
-  /** Unique ID of the user session. */
-  session_id: string;
-  /** Unique ID of the user. */
-  user_id?: string;
   /** Optional search terms. */
   query?: string;
+  /** Optional ID of the custom results that produced the current set of results. */
+  custom_results_id?: string | number;
+}
+
+/**
+ * Parameters for click stats without dfid.
+ * @public
+ */
+export interface ClickStatsParamsWithId extends StatsParams {
+  /** Unique ID of the clicked result as provided in the data source. */
+  id: string | number;
+  /** Data type of the clicked result. */
+  datatype: string;
+  /** Optional search terms. */
+  query?: string;
+  /** Optional ID of the custom results that produced the current set of results. */
+  custom_results_id?: string | number;
 }
 
 /**
@@ -55,8 +47,7 @@ export interface ClickStatsParamsWithDfid extends StatsParams {
  */
 export interface ImageStatsParams extends StatsParams {
   /** Unique ID of the image in Doofinder. */
-  id: string | number;
-  query?: string;
+  img_id: string | number;
 }
 
 /**
@@ -65,7 +56,9 @@ export interface ImageStatsParams extends StatsParams {
  */
 export interface RedirectionStatsParams extends StatsParams {
   /** Unique ID of the redirection in Doofinder. */
-  id: string | number;
+  redirection_id: string | number;
+  /** The target of the redirection. */
+  link: string;
   /** Optional search terms. */
   query?: string;
 }
@@ -76,15 +69,17 @@ export interface RedirectionStatsParams extends StatsParams {
  */
 export interface CartItemStatsParams extends StatsParams {
   /** Unique ID of the clicked result. */
-  id: string;
+  item_id: string | number;
   /** Amount of the given item **/
-  amount: number;
-  /** Name of the indice the item belongs to. i.e. "product" **/
-  index: string;
+  amount: string | number;
+  /** Datatype where the item can be found **/
+  datatype?: string;
   /** Title of the given item **/
-  title: string;
+  title?: string;
   /** Price of the given item **/
-  price: number;
+  price?: number;
+  /** Optional ID of the custom results that produced the current set of results. */
+  custom_results_id?: string | number;
 }
 
 /**
@@ -115,22 +110,37 @@ export class StatsClient {
    *
    * @public
    */
-  public async registerSession(params: InitParams): Promise<Response> {
-    return this.client.stats('init', params as Record<string, any>, Method.PUT);
+  public async registerSession(params: StatsParams): Promise<Response> {
+    return this.client.stats('init', params as Record<string, any>);
   }
 
   /**
    * Register a click on a result.
    *
-   * @param params - An options object. See {@link ClickStatsParamsWithDfid}.
+   * @param params - An options object. See {@link ClickStatsParamsWithDfid} and {@link ClickStatsParamsWithId}.
    * @returns A promise to be fullfilled with the response or rejected
    * with a `ClientResponseError`.
    *
    * @public
    */
-  public async registerClick(params: ClickStatsParamsWithDfid): Promise<Response> {
-    validateRequired([params.dfid, params.hashid, params.session_id], 'dfid, hashid and session_id are required');
-    return this.client.stats('click', params as Record<string, any>, Method.PUT);
+  public async registerClick(params: ClickStatsParamsWithDfid | ClickStatsParamsWithId): Promise<Response> {
+    const options = clone(params);
+    if ('dfid' in options) {
+      validateDoofinderId(options.dfid);
+      /* eslint-disable @typescript-eslint/ban-ts-ignore */
+      // @ts-ignore
+      delete options.id;
+      // @ts-ignore
+      delete options.datatype;
+      /* eslint-enable @typescript-eslint/ban-ts-ignore */
+    } else {
+      validateRequired([options.id, options.datatype], 'dfid or id + datatype are required');
+      /* eslint-disable @typescript-eslint/ban-ts-ignore */
+      // @ts-ignore
+      delete options.dfid;
+      /* eslint-enable @typescript-eslint/ban-ts-ignore */
+    }
+    return this.client.stats('click', options as Record<string, any>);
   }
 
   /**
@@ -142,8 +152,8 @@ export class StatsClient {
    *
    * @public
    */
-  public async registerCheckout(params: CheckoutParams): Promise<Response> {
-    return this.client.stats('checkout', params as Record<string, any>, Method.PUT);
+  public async registerCheckout(params: StatsParams): Promise<Response> {
+    return this.client.stats('checkout', params as Record<string, any>);
   }
 
   /**
@@ -156,8 +166,8 @@ export class StatsClient {
    * @public
    */
   public async registerImageClick(params: ImageStatsParams): Promise<Response> {
-    validateRequired(params.id, 'id is required');
-    return this.client.stats('image', params as Record<string, any>, Method.PUT);
+    validateRequired(params.img_id, 'img_id is required');
+    return this.client.stats('img_click', params as Record<string, any>);
   }
 
   /**
@@ -170,8 +180,8 @@ export class StatsClient {
    * @public
    */
   public async registerRedirection(params: RedirectionStatsParams): Promise<Response> {
-    validateRequired([params.id], 'id is required');
-    return this.client.stats('redirect', params as Record<string, any>, Method.PUT);
+    validateRequired([params.redirection_id, params.link], 'redirection_id and link are required');
+    return this.client.stats('redirect', params as Record<string, any>);
   }
 
   /**
@@ -190,8 +200,8 @@ export class StatsClient {
    * @beta
    */
   public async addToCart(params: CartItemStatsParams): Promise<Response> {
-    validateRequired(params, 'id, amount, index, price and title are required');
-    return this.client.stats(`cart/${params.session_id}`, params as Record<string, any>, Method.PUT);
+    validateRequired([params.item_id, params.amount], 'item_id and amount are required');
+    return this.client.stats('add-to-cart', params as Record<string, any>);
   }
 
   /**
@@ -210,8 +220,8 @@ export class StatsClient {
    * @beta
    */
   public async removeFromCart(params: CartItemStatsParams): Promise<Response> {
-    validateRequired([params.id, params.amount, params.index], 'id, amount and index are required');
-    return this.client.stats(`cart/${params.session_id}`, params as Record<string, any>, Method.PATCH);
+    validateRequired([params.item_id, params.amount], 'item_id and amount are required');
+    return this.client.stats('remove-from-cart', params as Record<string, any>);
   }
 
   /**
@@ -224,7 +234,7 @@ export class StatsClient {
    * @beta
    */
   public async clearCart(params: StatsParams): Promise<Response> {
-    return this.client.stats(`cart/${params.session_id}`, params as Record<string, any>, Method.DELETE);
+    return this.client.stats('clear-cart', params as Record<string, any>);
   }
 
   /**
