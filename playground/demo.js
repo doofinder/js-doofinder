fb8149afb14c1c9696317592ccb70e72bf213cc9
Manuel <manuel.irazabal@doofinder.com>
2023-08-07 10:53:37 +0200

    Update library doophoenix v6 part3 (#314)
    Fixes: doofinder/doophoenix#1753
    Part 1: #312
    Part 2: #312
    
    Summary
    The problem arose during the hackathon of updating our JavaScript library that works with Doophenix V5 to version 6. Together with @davidmolinacano, we reviewed the new Doophenix API and managed to make all the endpoints work, but there is still a lot of work to be done. We created this issue to finish it.
    
    Update example files.

--- playground/demo.js
+++ playground/demo.js
@@ -1,39 +1,118 @@
 function demo({ Client, Query, StatsClient }) {
-  const hashid = '7a6100c782c09126479e9270ecc619b3';
-  const sessionId = '123456';
-  const client = new Client({ server: 'eu1-search.doofinder.com' });
-  const request = new Query({ hashid });
+  /** All requests should be done with https protocol in our api location. https://{search_zone}-search.doofinder.com where {search_zone} depends on your location, is the geographic zone your search engine is located at. i.e.: eu1.  (https://docs.doofinder.com/api/search/v6/#section/Basics/Endpoint) */
+  const server = 'eu1-search.doofinder.com'
+  /** Your personal authentication token obtained in the control panel. (https://docs.doofinder.com/api/search/v6/#section/Basics/Authentication) */
+  const secret = '';
+  /** The search engine's unique id. i.e.: d8fdeab7fce96a19d3fc7b0ca7a1e98b (https://docs.doofinder.com/api/search/v6/#section/Basics/Conventions) */
+  const hashid = ''
+  /** The current session ID, must be unique for each user. */
+  const sessionId = '';
+  /** Unique ID of the clicked result. */
+  const dfid = ''
+
+  const client = new Client({ server, secret });
+  const request = new Query({
+    hashid
+  });
   const stats = new StatsClient(client);
 
-  request.text = 'shoes';
-  request.filters.add('condition', 'new');
-  request.filters.add('color', 'Red');
-  request.queryCounter = 2;
+  request.text = 'Conga 9090 AI';
 
   console.log(request.dump());
 
   client.search(request).then(result => {
-    console.log(result);
+    console.log('SEARCH: ', result);
   });
 
   client.suggest(request).then(result => {
-    console.log(result);
+    console.log('SUGGEST: ', result);
   });
 
-  const params = {
+  const image =
+    '';
+
+  client.searchImage(request, image).then(result => {
+    console.log('SEARCH IMAGE: ', result);
+  });
+
+  let params = {
     hashid,
     session_id: sessionId,
-    item_id: 10,
+    user_id: "dd"
+  }
+
+  stats.registerSession(params).then((result) => {
+    console.log('INIT SESSION: ', result);
+  });
+
+  params = {
+    hashid,
+    session_id: sessionId,
+    id: '1'
+  }
+
+  stats.registerRedirection(params).then((result) => {
+    console.log('LOG REDIRECTION: ', result);
+  });
+
+  params = {
+    hashid,
+    session_id: sessionId,
+    dfid: dfid
+  }
+
+  stats.registerClick(params).then((result) => {
+    console.log('CLICK: ', result);
+  });
+
+  params = {
+    hashid,
+    session_id: sessionId,
+    id: `1`,
+    query: '',
+  }
+
+  stats.registerImageClick(params).then((result) => {
+    console.log('IMAGE CLICK: ', result);
+  });
+
+  params = {
+    hashid,
+    session_id: sessionId,
+    id: 10,
     amount: 1,
-    datatype: 'product',
+    index: '1',
     title: 'Nice stuff',
     price: 20,
-  };
+  }
 
-  stats.addToCart(params).then(() => {
-    stats.clearCart({
+  stats.addToCart(params).then((result) => {
+    console.log('ADD: ', result);
+
+    const params = {
       hashid,
       session_id: sessionId,
+      id: 10,
+      amount: 1,
+      index: '1',
+    }
+
+    stats.removeFromCart(params).then((result) => {
+      console.log('REMOVE: ', result);
+
+      stats.clearCart({ hashid, session_id: sessionId }).then((resultDelete) => {
+        console.log('CLEAR: ', resultDelete);
+      });
     });
   });
+
+  params = {
+    hashid,
+    session_id: sessionId,
+  }
+
+  stats.registerCheckout(params).then((result) => {
+    console.log('CHECKOUT: ', result);
+  });
+
 }
